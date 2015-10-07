27a4c292931036df2053e5f06301758fa4ee442d
geoceid123 <george@george-pc>
2015-10-08 00:21:03 +0200

    Initial

---
+++ myProject/src/main/java/geoceid/myProject/App.java
@@ -0,0 +1,13 @@
+package geoceid.myProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello geoceid!" );
+    }
+}
