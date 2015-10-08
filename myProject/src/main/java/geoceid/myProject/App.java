62477418c00a5592f2f19552fd8ccaef55d79f16
geoceid123 <george@george-pc>
2015-10-08 22:24:12 +0200

    1st feature

--- myProject/src/main/java/geoceid/myProject/App.java
+++ myProject/src/main/java/geoceid/myProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello geoceid!" );
+        System.out.println( "We start development for the next version!" );
     }
 }
